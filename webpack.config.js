f77725159d2783054c679aba177b3561dfddfe62
BensonLiao <Benson.Liao@androvideo.com>
2020-07-17 11:59:51 +0800

    refactor(webpack): replace indent to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
 const path = require('path');
 module.exports = {
-    mode: 'production',
-    entry: './src/Editable.jsx',
-    output: {
-        path: path.join(__dirname, './build'),
-        filename: 'index.js',
-        library: 'react-text-content-editable', 
-        libraryTarget: 'umd',
-        publicPath: '/build/',
-        umdNamedDefine: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    },
-    resolve: { 
-        alias: { 
-            'react': path.resolve(__dirname, './node_modules/react') ,
-            'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-        } 
-    },
-    externals: {
-        // Don't bundle react or react-dom
-        react: {
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "React",
-            root: "React"
-        },
-        "react-dom": {
-            commonjs: "react-dom",
-            commonjs2: "react-dom",
-            amd: "ReactDOM",
-            root: "ReactDOM"
+  mode: 'production',
+  entry: './src/Editable.jsx',
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: 'index.js',
+    library: 'react-text-content-editable', 
+    libraryTarget: 'umd',
+    publicPath: '/build/',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
         }
+      }
+    ]
+  },
+  resolve: { 
+    alias: { 
+      'react': path.resolve(__dirname, './node_modules/react') ,
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+    } 
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
     }
+  }
 };
