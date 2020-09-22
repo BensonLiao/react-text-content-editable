c8cdcb29773248efe5a422b070f5586a0f74dcfa
BensonLiao <skylock777@gmail.com>
2020-09-22 15:55:38 +0800

    fix(master): add util to get caret pos and improve on paste handler

--- src/Editable.js
+++ src/Editable.js
@@ -1,7 +1,35 @@
-import React, { createRef, useState } from "react";
-import PropTypes from "prop-types";
-import { isStyledComponent } from 'styled-components';
-import { Wrapper, RootWrapper, InputContainer, InputWrapper } from "./style";
+/* eslint-disable react/boolean-prop-naming */
+import React, {createRef, useState} from 'react';
+import PropTypes from 'prop-types';
+import {isStyledComponent} from 'styled-components';
+import {Wrapper, RootWrapper, InputContainer, InputWrapper} from './style';
+const getCaretPosition = editableDiv => {
+  let caretPos = 0;
+  let sel;
+  let range;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0);
+      if (range.commonAncestorContainer.parentNode === editableDiv) {
+        caretPos = range.endOffset;
+      }
+    }
+  } else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    if (range.parentElement() === editableDiv) {
+      const tempEl = document.createElement('span');
+      editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+      const tempRange = range.duplicate();
+      tempRange.moveToElementText(tempEl);
+      tempRange.setEndPoint('EndToEnd', range);
+      caretPos = tempRange.text.length;
+    }
+  }
+
+  return caretPos;
+};
+
 const Editable = ({
   type,
   onChange,
@@ -18,7 +46,7 @@ const Editable = ({
 }) => {
   const inputRef = innerRef || createRef();
   const [data] = useState(value);
-  const [borderBottom, setBorderBottom] = useState("2px solid white");
+  const [borderBottom, setBorderBottom] = useState('2px solid white');
   const [inputWidth, setInputWidth] = useState(width);
   const [InputHeight, setInputHeight] = useState(height);
   const [isOnFocus, setOnFocus] = useState(false);
@@ -38,26 +66,46 @@ const Editable = ({
       textRange.select();
     }
   };
+
+  const placeCaretAtPos = (el, pos = 0) => {
+    el.focus();
+    let sel;
+    if (window.getSelection) {
+      sel = window.getSelection();
+      const textNode = sel.focusNode.firstChild;
+      sel.collapse(textNode, Math.min(textNode.length, pos));
+    } else if (document.getSelection) {
+      sel = document.getSelection();
+      // IE <= 8
+      if (sel.type !== 'Control') {
+        const range = sel.createRange();
+        range.move('character', pos);
+        range.select();
+      }
+    }
+  };
+
   const onFocus = () => {
     setOnFocus(true);
     if (!readOnly) {
       setInputWidth(width);
       setInputHeight(height);
-      setBorderBottom("2px solid #1DA1F1");
+      setBorderBottom('2px solid #1DA1F1');
     }
   };
 
   const onBlur = () => {
     setOnFocus(false);
-    setBorderBottom("none");
-    setInputHeight("auto");
-    setInputWidth("auto");
+    setBorderBottom('none');
+    setInputHeight('auto');
+    setInputWidth('auto');
   };
+
   const onInput = e => {
-    const { textContent } = e.currentTarget;
-    const rem = +maxLength - inputRef.current.innerText.length;
+    const {textContent} = e.currentTarget;
+    const rem = Number(maxLength) - inputRef.current.innerText.length;
     if (rem <= 0) {
-      const slicedText = textContent.slice(0, +maxLength);
+      const slicedText = textContent.slice(0, Number(maxLength));
       inputRef.current.innerText = slicedText;
       placeCaretAtEnd(inputRef.current);
       onChange(slicedText);
@@ -65,21 +113,41 @@ const Editable = ({
       onChange(textContent);
     }
   };
+
   const onPaste = e => {
+    let caretPos = getCaretPosition(inputRef.current);
     const selection = window.getSelection && window.getSelection();
     const {textContent} = e.currentTarget;
     const pastedText = e.clipboardData.getData('text/plain') || '';
-    const newContent = textContent.substring(0, selection.anchorOffset || 0) +
+    // If anchorOffset is greater than focusOffset that means user are select from right to left
+    const selectionStart = selection.anchorOffset < selection.focusOffset ?
+      selection.anchorOffset : selection.focusOffset;
+    const selectionEnd = selection.anchorOffset < selection.focusOffset ?
+      selection.focusOffset : selection.anchorOffset;
+    const newContent = textContent.substring(0, selectionStart || 0) +
       pastedText +
-      textContent.substring(selection.focusOffset || 0);
-    const rem = +maxLength - newContent.length;
+      textContent.substring(selectionEnd || 0);
+    const rem = Number(maxLength) - newContent.length;
     if (rem <= 0) {
       e.preventDefault();
-      const mData = newContent.slice(0, +maxLength);
-      inputRef.current.innerText = mData;
-      placeCaretAtEnd(inputRef.current);
+      if (selection.type === 'Range') {
+        const mData = textContent.substring(0, selectionStart || 0) +
+          pastedText.slice(0, selectionEnd - selectionStart) +
+          textContent.substring(selectionEnd || 0);
+        inputRef.current.innerText = mData;
+      } else {
+        const availableSpace = Number(maxLength) - textContent.length;
+        const mData = textContent.substring(0, selectionStart || 0) +
+          pastedText.slice(0, availableSpace) +
+          textContent.substring(selectionEnd || 0);
+        inputRef.current.innerText = mData;
+        caretPos += availableSpace;
+      }
+
+      placeCaretAtPos(inputRef.current, caretPos);
     }
   };
+
   const MainWrapper = customWrapper && isStyledComponent(customWrapper) ? customWrapper : Wrapper;
   const CustomTag = `${tag}`;
   return (
@@ -88,19 +156,19 @@ const Editable = ({
         <InputContainer width={width}>
           <InputWrapper width={inputWidth} readOnly={readOnly} isOnFocus={isOnFocus} ellipseOnBlur={ellipseOnBlur}>
             <CustomTag
-              className={type}
               ref={inputRef}
+              className={type}
               contentEditable={!readOnly}
+              style={{
+                height: InputHeight === 'auto' ? 'auto' : `${InputHeight}px`,
+                border: borderBottom,
+                minWidth: minWidth
+              }}
+              dangerouslySetInnerHTML={{__html: data.replace(/\n/g, '<br/>')}}
               onFocus={onFocus}
               onBlur={onBlur}
               onInput={onInput}
               onPaste={onPaste}
-              style={{
-                height: InputHeight === "auto" ? "auto" : `${InputHeight}px`,
-                border: borderBottom,
-                minWidth: minWidth
-              }}
-              dangerouslySetInnerHTML={{ __html: data.replace(/\n/g, "<br/>") }}
             />
           </InputWrapper>
         </InputContainer>
@@ -110,11 +178,11 @@ const Editable = ({
 };
 
 Editable.defaultProps = {
-  type: "text",
-  value: "",
-  width: "auto",
-  height: "auto",
-  minWidth: "auto",
+  type: 'text',
+  value: '',
+  width: 'auto',
+  height: 'auto',
+  minWidth: 'auto',
   readOnly: false,
   ellipseOnBlur: false,
   innerRef: null,
