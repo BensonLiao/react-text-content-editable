820da9b57c7ddb7a6d1bf5c4cb5dedc5114744cf
BensonLiao <skylock777@gmail.com>
2020-10-06 11:09:24 +0800

    feat: disable the ime input when exceed max length

--- src/Editable.js
+++ src/Editable.js
@@ -3,6 +3,7 @@ import React, {createRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import {isStyledComponent} from 'styled-components';
 import {Wrapper, RootWrapper, InputContainer, InputWrapper} from './style';
+
 const getCaretPosition = editableDiv => {
   let caretPos = 0;
   let sel;
@@ -30,6 +31,37 @@ const getCaretPosition = editableDiv => {
   return caretPos;
 };
 
+const placeCaretAtPos = (el, pos = 0) => {
+  el.focus();
+  let sel;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    const textNode = sel.focusNode.firstChild || sel.focusNode;
+    sel.collapse(textNode, Math.min(textNode.length, pos));
+  } else if (document.getSelection) {
+    sel = document.getSelection();
+    // IE <= 8
+    if (sel.type !== 'Control') {
+      const range = sel.createRange();
+      range.move('character', pos);
+      range.select();
+    }
+  }
+};
+
+const imeInputDisableHandler = event => {
+  const sel = window.getSelection();
+  const textNode = sel.focusNode;
+  const caretPos = sel.focusOffset;
+  if (sel.type !== 'Range') {
+    event.preventDefault();
+    sel.removeAllRanges();
+    setTimeout(() => {
+      sel.collapse(textNode || event.target, caretPos);
+    }, 20);
+  }
+};
+
 const Editable = ({
   type,
   onChange,
@@ -51,30 +83,16 @@ const Editable = ({
   const [InputHeight, setInputHeight] = useState(height);
   const [isOnFocus, setOnFocus] = useState(false);
 
-  const placeCaretAtPos = (el, pos = 0) => {
-    el.focus();
-    let sel;
-    if (window.getSelection) {
-      sel = window.getSelection();
-      const textNode = sel.focusNode.firstChild;
-      sel.collapse(textNode, Math.min(textNode.length, pos));
-    } else if (document.getSelection) {
-      sel = document.getSelection();
-      // IE <= 8
-      if (sel.type !== 'Control') {
-        const range = sel.createRange();
-        range.move('character', pos);
-        range.select();
-      }
-    }
-  };
-
   const onFocus = () => {
     setOnFocus(true);
     if (!readOnly) {
       setInputWidth(width);
       setInputHeight(height);
       setBorderBottom('2px solid #1DA1F1');
+    }
+
+    if (value >= maxLength) {
+      inputRef.current.addEventListener('compositionstart', imeInputDisableHandler);
     }
   };
 
@@ -83,15 +101,18 @@ const Editable = ({
     setBorderBottom('none');
     setInputHeight('auto');
     setInputWidth('auto');
+    inputRef.current.removeEventListener('compositionstart', imeInputDisableHandler);
   };
 
   const onInput = e => {
+    inputRef.current.removeEventListener('compositionstart', imeInputDisableHandler);
     const selection = window.getSelection && window.getSelection();
     let caretPos = getCaretPosition(inputRef.current);
     const rem = Number(maxLength) - inputRef.current.innerText.length;
     if (rem < 0 && selection.type !== 'Range') {
       inputRef.current.innerText = value;
       caretPos = caretPos <= 1 ? caretPos : caretPos - 1;
+      inputRef.current.addEventListener('compositionstart', imeInputDisableHandler);
     } else {
       const {textContent} = e.currentTarget;
       inputRef.current.innerText = textContent;
